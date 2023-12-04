70ac90f220a039a177ffd7f24acb60837fd5d4a4
Ryan Gannon <ryanmgannon@gmail.com>
2023-12-04 18:00:12 -0500

    Day 3 scaffolding

---
+++ day3.lisp
@@ -0,0 +1,6 @@
+(in-package #:cl-user)
+(uiop:define-package #:aoc23/day3
+  (:nicknames #:day3)
+  (:use :cl :alexandria :serapeum :aoc23/util)
+  (:export :part1 :part2))
+(in-package :aoc23/day3)
