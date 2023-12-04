cb8d9ebfd430544b47551f58261049019145d039
Ryan Gannon <ryanmgannon@gmail.com>
2023-12-03 19:08:14 -0500

    Day 2 boilerplate

---
+++ day2.lisp
@@ -0,0 +1,6 @@
+(in-package #:cl-user)
+(uiop:define-package #:aoc23/day2
+  (:nicknames #:day2)
+  (:use :cl :alexandria :serapeum :aoc23/util)
+  (:export :part1 :part2))
+(in-package :aoc23/day2)
