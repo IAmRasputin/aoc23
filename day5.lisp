ab33fb868ffcf999ee9393308de672c673c375c7
Ryan Gannon <ryanmgannon@gmail.com>
2023-12-12 20:55:30 -0500

    5 skeleton

--- day5.lisp
+++ day5.lisp
@@ -7,3 +7,21 @@
 (in-package :aoc23/day5)
 
 (defvar *input* (input-lines 5))
+
+(defun input-seeds ()
+  (let ((numbers (string-trim " " (second (split-string (first *input*) :separator '(#\:))))))
+    (mapcar #'parse-integer (split-string numbers))))
+
+
+(defmacro compile-range (first-start second-start range-size)
+  `(lambda (input)
+     (when (<= ,first-start ,input ,(+ first-start range-size))
+       (+ ,second-start (- input ,first-start)))))
+
+(defun input-maps (from to)
+  (let* ((map-strings (cdr (split-sequence-if #'blankp *input*)))
+         ())
+    ))
+
+(defun part1 ()
+  )
